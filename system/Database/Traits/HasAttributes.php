4f7c67cd5e0f310ccb443ef0c82afd39d969a7b6
HosseinAjerloo <ahosseinajerloo@gmail.com>
2024-06-04 02:49:50 +0330

    compation arrayToAttributes method in trait HasAttribute

--- system/Database/Traits/HasAttributes.php
+++ system/Database/Traits/HasAttributes.php
@@ -3,32 +3,45 @@
 namespace System\Database\Traits;
 trait HasAttributes
 {
-    private function registerAttribute($key, $value)
+    private function registerAttribute($object, string $attribute, $value)
     {
-
+        $this->inCastsAttributes($attribute) == true ? $object->$attribute = $this->castDeCodeValue($attribute, $value) : $object->$attribute = $value;
     }
 
-    protected function arrayToAttributes()
-    {
+    protected function arrayToAttributes(array $array, $object = null)
 
+    {
+        if (!$object) {
+            $object = (new (get_called_class()));
+        }
+        foreach ($array as $attribute => $value) {
+            if ($this->inHiddenAttributes($attribute))
+                continue;
+            $this->registerAttribute($object, $attribute, $value);
+        }
+        return $object;
     }
+
     protected function arrayToObject()
     {
 
     }
-    protected function inHiddenAttributes()
-    {
 
+    protected function inHiddenAttributes($attribute): bool
+    {
+        return false;
     }
-    private function inCastsAttributes()
-    {
 
+    private function inCastsAttributes($attribute): bool
+    {
+        return false;
     }
 
     private function castDeCodeValue()
     {
 
     }
+
     protected function castEnCodeValue()
     {
 
