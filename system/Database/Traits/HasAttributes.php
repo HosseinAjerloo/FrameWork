5e5bb3879810c444e1dc3455726f7211f66dd1c7
HosseinAjerloo <ahosseinajerloo@gmail.com>
2024-06-03 22:06:24 +0330

    start hasquery builder attribute traie file

--- system/Database/Traits/HasAttributes.php
+++ system/Database/Traits/HasAttributes.php
@@ -1,6 +1,41 @@
 <?php
+
 namespace System\Database\Traits;
 trait HasAttributes
 {
+    private function registerAttribute($key, $value)
+    {
 
+    }
+
+    protected function arrayToAttributes()
+    {
+
+    }
+    protected function arrayToObject()
+    {
+
+    }
+    protected function inHiddenAttributes()
+    {
+
+    }
+    private function inCastsAttributes()
+    {
+
+    }
+
+    private function castDeCodeValue()
+    {
+
+    }
+    protected function castEnCodeValue()
+    {
+
+    }
+
+    private function arrayToCastEnCodeValue()
+    {
+
+    }
 }
